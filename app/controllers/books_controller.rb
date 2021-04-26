ca2a3d74543a6cef2a7e6b6cec466306fcc45e1a
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-04-26 10:56:42 +0900

    新規投稿フォーム・機能を実装しビューを修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,27 @@
 class BooksController < ApplicationController
 
   def index
+    @books = Book.all
   end
   
   def new
+    @book = Book.new
   end
 
   def create
+    binding.pry
+    @book = Book.new(book_params)
+    if @book.valid? 
+      @book.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
+  private
+
+  def book_params
+    params.require(:book).permit(:text,:quote,:title,:title_option, :author,:publish,:year,:page).merge(user_id: params[:format])
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,12 +2,12 @@ class Book < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
-    balidates :text,length: { minimum: 1, maximum: 140 }
-    balidates :quote,length: { minimum: 1, maximum: 140 }
-    balidates :title
-    balidates :author
-    balidates :publish
-    balidates :yeare, numericality: {only_integer: true} 
-    balidates :page, numericality: {only_integer: true} 
+    validates :text,length: { minimum: 1, maximum: 140 }
+    validates :quote,length: { minimum: 1, maximum: 140 }
+    validates :title
+    validates :author
+    validates :publish
+    validates :year, numericality: {only_integer: true} 
+    validates :page, numericality: {only_integer: true} 
   end
 end
