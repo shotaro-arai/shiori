84c887b6918fe271203275b98342a9460b96681d
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-04-27 15:46:27 +0900

    yarn アップデートに伴い発生したsign_out[GET]エラーの解決法

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require ("jquery_ujs") 
 require("../index")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
