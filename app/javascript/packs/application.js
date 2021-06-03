c3bd052518e0941528f603c9d5215e71943eafb4
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-06-03 12:40:53 +0900

    method: :deleteが、getメソッドのルーティングエラーのため読み込み順の変更
    ※未解決

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,13 +9,12 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 
+import 'bootstrap'
+import '../stylesheets/application'
+
 require("../index")
 require("../comment")
 require("../preview")
-
-import 'bootstrap';
-import '../stylesheets/application';
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
