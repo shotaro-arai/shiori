3be0b099d3decbf05244fb31abbda8b1834ecb82
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-06-08 20:38:20 +0900

    bootstrapではなくJavaScriptでモーダルウインドウを実装する方向性に変更。関連する記述を削除・アンインストール
    ビュー崩れのため

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,8 +5,7 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery',
-        Popper: 'popper.js'
+        jQuery: 'jquery/src/jquery'
     })
 )
 
